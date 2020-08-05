94101e0dc4f6af128557cbe3e803e5ab0b188ba3
BuTn <kimmosc2@163.com>
2020-08-05 10:52:38 +0800

    fix deadlock

--- project/crawler/scheduler/sample.go
+++ project/crawler/scheduler/sample.go
@@ -13,6 +13,5 @@ func (s *SimpleScheduler) ConfigureMasterWorkerChan(c chan engine.Request) {
 }
 
 func (s *SimpleScheduler) Submit(r engine.Request) {
-	s.workerChan <- r
+	go func() { s.workerChan <- r }()
 }
-
